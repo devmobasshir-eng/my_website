2aa83f0b605b8622f7aba7ca021263b60e013ee2
mobasshir <dev.mobasshir@gmail.com>
2026-08-07 18:02:50 +0530

    Updated project files

--- web.js
+++ web.js
@@ -3,6 +3,7 @@ let reset = document.querySelector("#box");
 let winnerList = document.querySelector("#winner");
 let NewButton = document.querySelector("#newButton");
 let massegContener = document.querySelector(".mas-contener");
+let drawContener = document.querySelector(".draw-contener");
 
 let turn = true;
 const win = [
@@ -14,19 +15,20 @@ const win = [
     [2, 4, 6],
     [3, 4, 5],
     [6, 7, 8],
-] ;
+];
 
 const resetGame = () => {
-         turn = true;
-         enableButton();
-         massegContener.classList.add("hide");
+    turn = true;
+    enableButton();
+    massegContener.classList.add("hide");  // winner hide
+    drawContener.classList.add("hide");    // draw hide
 };
 
 const disableButton = () => {
-          for( let box of button) {
-            box.disabled = true;
-          }
-          };
+    for (let box of button) {
+        box.disabled = true;
+    }
+};
 
 button.forEach((box) => {
     box.addEventListener("click", () => {
@@ -39,36 +41,59 @@ button.forEach((box) => {
         }
         box.disabled = true;
         checkwin();
-});
+    });
 });
 
-  const enableButton = () => {
+const enableButton = () => {
     for (let box of button) {
         box.disabled = false;
         box.innerText = "";
     }
-  }
-         
+};
 
 const showWinner = (winner) => {
-         winnerList.innerText = `Congrutulation,Winner is ${winner}`;
-         massegContener.classList.remove("hide");
-         disableButton();
-}
+    winnerList.innerText = `Congratulation, Winner is ${winner}`;
+    massegContener.classList.remove("hide");
+    disableButton();
+};
+
+const showDraw = () => {
+    drawContener.classList.remove("hide");
+    disableButton();
+};
 
 const checkwin = () => {
-    for (let pattern of win) {
-           let posVlu = button[pattern[0]].innerText;
-           let posVlu2 = button[pattern[1]].innerText;
-            let posVlu3 = button[pattern[2]].innerText;
+    let winnerfound = false;
 
-         if(posVlu!="" && posVlu2 !="" && posVlu3 !="") {
-            if(posVlu === posVlu2 && posVlu2 === posVlu3) {
-              console.log("winner",posVlu);  
-              showWinner(posVlu);
+    for (let pattern of win) {
+        let posVlu = button[pattern[0]].innerText;
+        let posVlu2 = button[pattern[1]].innerText;
+        let posVlu3 = button[pattern[2]].innerText;
+
+        if (posVlu != "" && posVlu2 != "" && posVlu3 != "") {
+            if (posVlu === posVlu2 && posVlu2 === posVlu3) {
+                console.log("winner", posVlu);
+                showWinner(posVlu);
+                winnerfound = true;
+                break; // winner mil gaya, loop stop
             }
-         }
+        }
     }
-}
-NewButton.addEventListener("click",resetGame);
-reset.addEventListener("click",resetGame);
+
+    // Agar winner nahi mila tabhi draw check hoga
+    if (!winnerfound) {
+        let allFilled = true;
+        for (let box of button) {
+            if (box.innerText === "") {
+                allFilled = false;
+                break;
+            }
+        }
+        if (allFilled) {
+            showDraw();
+        }
+    }
+};
+
+NewButton.addEventListener("click", resetGame);
+reset.addEventListener("click", resetGame);
